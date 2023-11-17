15a771b8444702ee06a12a742d1281c375d164d4
Om Janamanchi <93719948+10020849@users.noreply.github.com>
2023-11-16 21:14:27 -0500

    Add files via upload

--- CallableThreadCount.java
+++ CallableThreadCount.java
@@ -1,78 +1,32 @@
-import java.time.Instant;
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.*; import java.time.Instant; import java.util.concurrent.*;
 public class CallableThreadCount implements Callable<Long>
 {
-    private final int threads = 1000000;
-    private int threadNum;
-
-    public CallableThreadCount()
-    {
-        System.out.println("In thread");
-        threadNum = 0;
-    }
-
+    int threadNum;
+    public CallableThreadCount() { System.out.println("In thread"); }
     public Long call()
     {
-        for (int i = 0; i < threads; i++)
-        {
+        for (int i = 0; i < 1000000; i++)
             threadNum++;
-        }
         return (long) threadNum;
     }
-
     public static void main(String[] args)
     {
-        // Number of threads
         int numThreads = 100;
-
-        // Create a list to hold Callable tasks
         List<Callable<Long>> tasks = new ArrayList<>();
-
         for (int i = 0; i < numThreads; i++)
-        {
             tasks.add(new CallableThreadCount());
-        }
-
-        // Create an ExecutorService with a fixed thread pool
-        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
-
-        // Record the start time
-        Instant startTime = Instant.now();
-        int epoch1 = startTime.getNano();
-
+        ExecutorService es = Executors.newFixedThreadPool(numThreads);
+        Instant start = Instant.now();
         try
         {
-            // Submit all tasks and store the futures in a list
-            List<Future<Long>> futures = executorService.invokeAll(tasks);
-
-            // Process the results
+            List<Future<Long>> futures = es.invokeAll(tasks);
             long totalSum = 0;
             for (Future<Long> future : futures)
-            {
-                // Retrieve the result from each task and sum them up
                 totalSum += future.get();
-            }
-
-            // Print the total sum
-            System.out.println("Total Sum: " + totalSum);
-        }
-        catch (InterruptedException | ExecutionException e)
-        {
-            e.printStackTrace();
-        }
-        finally
-        {
-            // Shut down the executor service
-            executorService.shutdown();
-        }
-
-        // Record the end time
-        Instant endTime = Instant.now();
-        int epoch2 = endTime.getNano();
-
-        // Time between start to end
-        double timeInSec = (double) (epoch2 - epoch1)/1000000000;
-        System.out.println("Time: " + timeInSec + " seconds");
+            System.out.println("Sum: " + totalSum);
+        } catch (Exception e) { e.printStackTrace(); }
+        es.shutdown();
+        Instant end = Instant.now();
+        System.out.println("Time: " + ( (double) (end.getNano() - start.getNano())/1000000000 ) + " seconds");
     }
 }
